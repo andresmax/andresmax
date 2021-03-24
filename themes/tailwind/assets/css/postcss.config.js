fb5a2cb9ce1f1b3c18d97feefb8f9e8ec221eea7
Andres Max <andresmax@gmail.com>
2021-03-24 06:51:14 -0500

    bleg

--- themes/tailwind/assets/css/postcss.config.js
+++ themes/tailwind/assets/css/postcss.config.js
@@ -13,15 +13,32 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
     }
 })
 
-module.exports = {    
-    plugins: [        
-        require('postcss-import')({
-            path: [themeDir]
-            }), 
-        require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'),
-        require('autoprefixer')({
-            path: [themeDir]
-        }),
-        ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
-    ]
-}
+//console.log("NODE_ENV", process.env.NODE_ENV);
+module.exports = {
+	plugins: [
+		require("tailwindcss")("./themes/tailwind/assets/css/tailwind.config.js"),
+		require("autoprefixer")({
+			grid: false,
+			browsers: [">1%"]
+		}),
+		...(process.env.NODE_ENV !== "development"
+			? [
+					require("@fullhuman/postcss-purgecss")({
+						content: ["./layouts/**/*.html", "./components/**/**/*.html"],
+						extractors: [
+							{
+								extractor: class {
+									static extract(content) {
+										//console.log(process.env.NODE_ENV);
+										//return content.match(/[a-zA-Z0-9-:_/]+/g) || [];
+										return content.match(/[A-z0-9-:\/]+/g) || [];
+									}
+								},
+								extensions: ["vue", "html"]
+							}
+						]
+					})
+			  ]
+			: []) //If Development, do not use PurgeCSS
+	]
+};
